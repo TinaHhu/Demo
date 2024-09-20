6d5005ec5a1367364ba625044379559a12ed1853
tinahhu <tinahhu@tencent.com>
2024-09-20 18:05:35 +0800

    搭建react环境

---
+++ react-study/src/Pages/App/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function IndexPage(){
+    return (
+        <>
+            <h1>
+                
+            </h1>
+        </>
+    )
+}
---
+++ react-study/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import IndexPage from './Pages/App/app';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <IndexPage />,
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
