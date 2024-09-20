b363c9d13ed3117d2dcc5934489605b9246bada3
tinahhu <tinahhu@tencent.com>
2024-09-20 20:01:10 +0800

    环境

--- react-study/src/Pages/App/app.jsx
+++ react-study/src/Pages/App/app.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-export default function IndexPage(){
+// 子组件
+export function IndexPage({person}){
     return (
         <>
             <h1>
-                
+                niaho
             </h1>
+            <p>{person.name}</p>
         </>
     )
 }
+
+// 父组件
+export default function Profile() {
+    return (
+      <IndexPage
+        person={{ 
+            name: 'Katsuko Saruhashi', 
+        }}
+      />
+    );
+  }
